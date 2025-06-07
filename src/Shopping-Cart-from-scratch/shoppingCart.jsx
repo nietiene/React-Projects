b2413df95bb5e6dffb4f5e210f40468b679ceeeb
nietiene <niyomugaboetiene53@gmail.com>
2025-06-07 10:12:58 -0700

    React js project

--- src/Shopping-Cart-from-scratch/shoppingCart.jsx
+++ src/Shopping-Cart-from-scratch/shoppingCart.jsx
@@ -55,10 +55,10 @@ const ShoppingCart = () => {
 
     return (
         <div className="max-w-6xl mx-auto p-4">
-          <h1 className="text-3xl font-bold text-center mb-6">🛒Shopping Cart</h1>
+          <h1 className="text-3xl font-bold text-center mb-6 font-bold text-blue-500">🛒Shopping Cart</h1>
           <div className="mb-6">
-          <h2 className="text-center block bg-blue-500">Products In Store</h2>
-          <p className="text-blue">Search Products</p>
+          <h2 className="text-center block text-2xl text-blue-500">Products In Store</h2>
+          <p className="text-blue-500 font-lg font-medium mb-2">Search Products</p>
           </div>
           <input type="search"  value={query} 
           onChange={search}
