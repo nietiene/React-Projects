45e995b5a00bf61727be7718ff0b0059a01cfac4
nietiene <niyomugaboetiene53@gmail.com>
2025-06-08 17:46:33 -0700

    React js project with styling

--- src/Shopping-Cart/Shopping.jsx
+++ src/Shopping-Cart/Shopping.jsx
@@ -34,7 +34,7 @@ function Shopping (){
 
     return (
         <div className="bg-green-200 shadow-xl rounded-2xl p-8 min-h-screen text-green-500">
-            <h2 className="text-green-600 text-2xl text-center font-bold">Shopping Cart</h2>
+            <h2 className="text-3xl text-center font-extrabold mb-6">Shopping Cart</h2>
             <h3 className="text-center font-medium text-green-500">Products</h3>
             {Products.map((products) => (
                    <div key={products.id}
