8c28560377cb0a94519f33b61e0fd0bee2378f37
nietiene <niyomugaboetiene53@gmail.com>
2025-06-10 12:16:38 -0700

    this is commit of react js styling

--- src/Adding-Product-To-Cart/Product.jsx
+++ src/Adding-Product-To-Cart/Product.jsx
@@ -39,10 +39,10 @@ const ShoppingCart = () => {
         <h2 className="font-extrabold text-blue-500 text-xl text-center underline">Products</h2>
         {products.map((products) => (
             <div key={products.id} 
-            className="grid sm:grid-cols-1 grid-cols md:grid-cols-2 md:grid-cols-3 p-2 bg-blue-300 shadow-lg ">
+            className="m-5 grid sm:grid-cols-1 grid-cols md:grid-cols-2 md:grid-cols-3 p-2 bg-blue-300 shadow-lg ">
                 <span className="text-xl p-2 text-blue-600">{products.name} - ${products.price.toFixed(2)}</span>
                 <button onClick={() => addToCart(products)}
-                    className="bg-blue-500 w-[40%] ms-[55%] rounded-lg shadow-lg text-white">
+                    className="bg-blue-500 w-[40%] ms-[160%] rounded-lg shadow-lg text-white hover:bg-blue-600">
                     Add To Cart
                 </button>
             </div>
