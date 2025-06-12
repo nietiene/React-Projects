55599ebe1ab0dd42d54776461dab1ac9a31ffc6c
nietiene <niyomugaboetiene53@gmail.com>
2025-06-11 17:37:11 -0700

    this is commit of react js styling

--- src/DropDown Menus/Key Activation/key_activation.jsx
+++ src/DropDown Menus/Key Activation/key_activation.jsx
@@ -21,7 +21,7 @@ const Key_Activation = () => {
              <button onClick={toggleDropDown} className="border border-yellow-900 p-2 mt-5 hover:bg-yellow-600">Toggle Dropdown</button>   
              {isDropDonwOpen && (
                 <ul className="font-bold bg-yellow-600 mt-5 w-[10%] text-center">
-                    <li>Option 1</li>
+                    <li className="hover:text-gray-600 hover:underline transition duration-200">Option 1</li>
                     <li>Option 2</li>
                     <li>Option 3</li>
                 </ul>
