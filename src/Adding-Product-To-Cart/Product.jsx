2391bc4e92a2a5e77584a7774b145d92ba22e4a1
nietiene <niyomugaboetiene53@gmail.com>
2025-06-11 17:09:32 -0700

    this is commit of react js styling

--- src/Adding-Product-To-Cart/Product.jsx
+++ src/Adding-Product-To-Cart/Product.jsx
@@ -58,7 +58,8 @@ const ShoppingCart = () => {
                     <li key={item.id} 
                     className="flex justify-between border border-blue-500 p-3 hover:bg-blue-600 hover:text-white transition duration">
                        <span className="text-blue-400">{item.name}</span> <span className="text-blue-400">{item.quantity}</span> <span className="text-blue-400"> ${item.price * item.quantity}</span>
-                        <button onClick={() => RemoveProduct(item.id)}>Remove</button>
+                        <button onClick={() => RemoveProduct(item.id)}
+                            className="bg-red-500 py-2 px-4 text-white">Remove</button>
                     </li>
                 ))}
             </ul>
