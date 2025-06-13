1a94cad6ce7cc13aa517b78034d977def035ae3a
nietiene <niyomugaboetiene53@gmail.com>
2025-06-12 17:28:25 -0700

    this is commit of react js styling

--- src/Ecomerce-Frontend/Home.jsx
+++ src/Ecomerce-Frontend/Home.jsx
@@ -7,9 +7,10 @@ export default function Home(){
     const {AddToCart} = useCart();
     return (
         <div className="bg-blue-400 min-h-screen">
-            <h1 className="text-center font-bold text-2xl text-white">Products</h1>
+            <h1 className="ms-3 font-bold text-2xl text-white">Products</h1>
             {Products.map((product) => (
-                <div key={product.id}>
+                <div key={product.id}
+                className="">
                   <h2>{product.name}</h2>
                   <p>{product.price}</p>
                   <button onClick={() => AddToCart(product)}>
