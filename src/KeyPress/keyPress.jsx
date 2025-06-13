4350e63aa6ff6efc0ea79cff0159a8c1a88cadd6
nietiene <niyomugaboetiene53@gmail.com>
2025-06-12 18:21:56 -0700

    this is commit of react js styling

--- src/KeyPress/keyPress.jsx
+++ src/KeyPress/keyPress.jsx
@@ -17,7 +17,7 @@ const KeyPress = () => {
    return (
     <div className="bg-blue-300 min-h-screen ">
         <h2 className="text-white text-2xl font-bold underline text-center">Reacting Key Presses</h2>
-        <p className="text-blue-500 font-xl">Last key pressed: {keyPressed}</p>
+        <p className="text-blue-500 text-xl ">Last key pressed: {keyPressed}</p>
     </div>
    )
     }
